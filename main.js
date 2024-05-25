f0c871fea249e53478dde098964a0907abe0d7cc
AlexNesvit <alex.nesvit2000@gmail.com>
2024-05-25 21:01:37 +0200

    background2

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     if (recipeForm) {
         recipeForm.style.opacity = '0';
         setTimeout(function() {
-            recipeForm.style.transition = 'opacity 0.5s ease-in-out';
+            recipeForm.style.transition = 'opacity 3.5s ease-in-out';
             recipeForm.style.opacity = '1';
         }, 500);
     }
